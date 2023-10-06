5b3fa8eb055f9022453294748b0701911227ad16
akash gangwar <akashgangwar4648@gmail.com>
2023-10-06 12:56:00 +0530

    portfolio

---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Teach from './Tech';
+import Experience from './Experience';
+import Works from './Works';
+import Contact from './Contact';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Teach,
+  Experience,
+  Works,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
---
+++ src/constants/index.js
@@ -0,0 +1,192 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    carrent,
+    restaurant,
+    data_visualization,
+    tripguide,
+    Ongraph,
+    Abesit,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Internship as FullStack Developer",
+      company_name: "Ongraph",
+      icon: Ongraph,
+      iconBg: "#383E56",
+      date: "26 June 2023 -4 August 2023",
+      points: [
+        "Developing a web applications using css tailwind and other related technologies.",
+        "Collaborating with cross-functional teams including designers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Frontend Web development course",
+      company_name: "Abesit",
+      icon: Abesit,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        "Done a Frontend web development course in college in which I learn to use HTML, CSS, JS",
+        "In this course I develop various websites using HTML, CSS, JS and other related technologies",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Also build various project during the course with other students as team",
+      ],
+    },
+
+  ];
+
+  
+  const projects = [
+    {
+      name: "Data Visualization",
+      description:
+        "It is a website which help user to visualize his data and also tells him about the data",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: data_visualization,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "website for Restaurant",
+      description:
+        "It is a single page website which is responsive and interactive",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: restaurant,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, projects };
